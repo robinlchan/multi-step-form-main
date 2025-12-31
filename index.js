721282a22a200c64d706da733e5305241bc6512c
Robin Chan <robinasaurousrex@gmail.com>
2025-12-31 15:33:40 -0700

    continue working on step 3 to 4 validation logic

--- index.js
+++ index.js
@@ -34,6 +34,18 @@ const phoneInput = document.querySelector("#phone");
 const nameError = document.querySelector("#name-error");
 const emailError = document.querySelector("#email-error");
 const phoneError = document.querySelector("#phone-error");
+// Prices
+const arcadeM = 9;
+const arcadeY = 90;
+const advancedM = 12;
+const advancedY = 120;
+const proM = 15;
+const proY = 150;
+const price = document.querySelector("#subscription-price");
+const add = document.querySelectorAll(".add");
+const planAddOns = document.querySelectorAll(".plan-selection");
+const cost = document.getElementById("total-cost");
+let total = 0;
 
 form.setAttribute("novalidate", "");
 form.addEventListener("submit", (e) => {
@@ -83,7 +95,7 @@ function prevStep() {
     mobileShowNum(currentNum);
   }
 }
-// Step 1
+// STEP 1
 function validateStep1() {
   const nameValue = nameInput.value;
   const emailValue = emailInput.value;
@@ -146,41 +158,70 @@ function validateStep1() {
   }
   return isValid;
 }
-// Step 2
+// STEP 2
 function validateStep2() {
-  function showError(plan) {
-    plan.style.border = "1px solid var(--primary-red-500)";
-  }
-  function removeError(plan) {
-    plan.style.border = "";
-  }
+  const subscription = document.querySelector(".subscription"); // <h2> step 3
+  const proMonthly = document.querySelector("#pro-monthly");
+  const proYearly = document.querySelector("#pro-yearly");
+  const advMonthly = document.querySelector("#adv-monthly");
+  const advYearly = document.querySelector("#adv-yearly");
+  const arcMonthly = document.querySelector("#arc-monthly");
+  const arcYearly = document.querySelector("#arc-yearly");
 
-  plans.forEach((plan) => {
-    plan.addEventListener("click", () => {
-      if (plan === arcade) {
-        arcade.classList.add("selected-plan");
-        advanced.classList.remove("selected-plan");
-        pro.classList.remove("selected-plan");
-        removeError(arcade);
-        removeError(advanced);
-        removeError(pro);
-      } else if (plan === advanced) {
-        advanced.classList.add("selected-plan");
-        arcade.classList.remove("selected-plan");
-        pro.classList.remove("selected-plan");
-        removeError(arcade);
-        removeError(advanced);
-        removeError(pro);
-      } else {
-        pro.classList.add("selected-plan");
-        arcade.classList.remove("selected-plan");
-        advanced.classList.remove("selected-plan");
-        removeError(arcade);
-        removeError(advanced);
-        removeError(pro);
-      }
-    });
-  });
+  // Arcade
+  if (
+    arcade.classList.contains("selected-plan") &&
+    arcMonthly.classList.contains("hidden")
+  ) {
+    subscription.innerText = "Arcade (Yearly)";
+    price.innerText = `$${arcadeY}/yr`;
+    cost.innerText = `$${arcadeY}/yr`;
+    total = arcadeY;
+  } else if (
+    arcade.classList.contains("selected-plan") &&
+    arcYearly.classList.contains("hidden")
+  ) {
+    subscription.innerText = "Arcade (Monthly)";
+    price.innerText = `$${arcadeM}/mo`;
+    cost.innerText = `$${arcadeM}/mo`;
+    total = arcadeM;
+  }
+  // Advanced
+  if (
+    advanced.classList.contains("selected-plan") &&
+    advMonthly.classList.contains("hidden")
+  ) {
+    subscription.innerText = "Advanced (Yearly)";
+    price.innerText = `$${advancedY}/yr`;
+    cost.innerText = `$${advancedY}/yr`;
+    total = advancedY;
+  } else if (
+    advanced.classList.contains("selected-plan") &&
+    advYearly.classList.contains("hidden")
+  ) {
+    subscription.innerText = "Advanced (Monthly)";
+    price.innerText = `$${advancedM}/mo`;
+    cost.innerText = `$${advancedM}/mo`;
+    total = advancedM;
+  }
+  // Pro
+  if (
+    pro.classList.contains("selected-plan") &&
+    proMonthly.classList.contains("hidden")
+  ) {
+    subscription.innerText = "Pro (Yearly)";
+    price.innerText = `$${proY}/yr`;
+    cost.innerText = `$${proY}/yr`;
+    total = proY;
+  } else if (
+    pro.classList.contains("selected-plan") &&
+    proYearly.classList.contains("hidden")
+  ) {
+    subscription.innerText = "Pro (Monthly)";
+    price.innerText = `$${proM}/mo`;
+    cost.innerText = `$${proM}/mo`;
+    total = proM;
+  }
 
   let isValid = true;
   if (
@@ -199,36 +240,102 @@ function validateStep2() {
   }
   return isValid;
 }
-// Step 3
+// STEP 3
 function validateStep3() {
-  const subPrice = document.getElementById("subscription-price"); //price <p>
-  const subscription = document.querySelector(".subscription"); // <h2> step 3
+  // Add-ons costs (mo/yr)
+  const addOneM = 1;
+  const addTwoM = 2;
+  const addOneY = 10;
+  const addTwoY = 20;
 
-  let isMonthly = true;
-  if (arcade.classList.contains("selected-plan, monthly-sub")) {
-    subPrice.innerText = "$9/mo";
-    subscription.innerText = "Arcade (Monthly)";
-  } else if (arcade.classList.contains("selected-plan, yearly-sub")) {
-    subPrice.innerText = "$90/yr";
-    subscription.innerText = "Arcade (Yearly)";
-    isMonthly = false;
-  } else if (advanced.classList.contains("selected-plan, monthly-sub")) {
-    subPrice.innerText = "$12/mo";
-    subscription.innerText = "Advanced (Monthly)";
-  } else if (advanced.classList.contains("selected-plan, yearly-sub")) {
-    subPrice.innerText = "$120/yr";
-    subscription.innerText = "Advanced (Yearly)";
-    isMonthly = false;
-  } else if (pro.classList.contains("selected-plan, monthly-sub")) {
-    subPrice.innerText = "$15/mo";
-    subscription.innerText = "Pro (Monthly)";
-  } else if (advanced.classList.contains("selected-plan, yearly-sub")) {
-    subPrice.innerText = "$150/yr";
-    subscription.innerText = "Pro (Yearly)";
-    isMonthly = false;
+  // Setting prices for add-ons
+  const onlineM = document.querySelector("#online-monthly");
+  const onlineY = document.querySelector("#online-yearly");
+  const storageM = document.querySelector("#storage-monthly");
+  const storageY = document.querySelector("#storage-yearly");
+  const profileM = document.querySelector("#profile-monthly");
+  const profileY = document.querySelector("#profile-yearly");
+  onlineM.innerText = `$${addOneM}/mo`;
+  onlineY.innerText = `$${addOneY}/yr`;
+  storageM.innerText = `$${addTwoM}/mo`;
+  storageY.innerText = `$${addTwoY}/yr`;
+  profileM.innerText = `$${addTwoM}/mo`;
+  profileY.innerText = `$${addTwoY}/yr`;
+  // Step 3 list of add-on to select
+  const online = document.getElementById("service");
+  const storage = document.getElementById("storage");
+  const customize = document.getElementById("profile");
+  // Step 4 list of add-ons
+  const onlineList = document.getElementById("online");
+  const storageList = document.getElementById("larger");
+  const customizeList = document.getElementById("customize");
+  // Spans that show price of add-ons on Step 4
+  const pricesCostM = document.querySelectorAll(".monthly-cost");
+  const pricesCostY = document.querySelectorAll(".yearly-cost");
+
+  if (online.checked === true) {
+    online.classList.add("selected-add");
+    onlineList.classList.remove("hidden");
+    onlineList.classList.add("selected");
+  } else {
+    online.classList.remove("selected-add");
+    onlineList.classList.add("hidden");
+    onlineList.classList.remove("selected");
   }
-  return isMonthly;
+
+  if (storage.checked === true) {
+    storage.classList.add("selected-add");
+    storageList.classList.remove("hidden");
+    storageList.classList.add("selected");
+  } else {
+    storage.classList.remove("selected-add");
+    storageList.classList.add("hidden");
+    storageList.classList.remove("selected");
+  }
+
+  if (customize.checked === true) {
+    customize.classList.add("selected-add");
+    customizeList.classList.remove("hidden");
+    customizeList.classList.add("selected");
+  } else {
+    customize.classList.remove("selected-add");
+    customizeList.classList.add("hidden");
+    customizeList.classList.remove("selected");
+  }
+
+  function isMonthly() {
+    pricesCostM.forEach((cost) => {
+      cost.classList.remove("hidden");
+    });
+    pricesCostY.forEach((cost) => {
+      cost.classList.add("hidden");
+    });
+  }
+  function isYearly() {
+    pricesCostY.forEach((cost) => {
+      cost.classList.remove("hidden");
+    });
+    pricesCostM.forEach((cost) => {
+      cost.classList.add("hidden");
+    });
+  }
+  // on step 4, listing prices of each add on to our total cost
+  // add is our list of spans that have the prices for step 4 summary
+  add.forEach((addOn) => {
+    if (
+      addOn.classList.contains("monthly-cost") &&
+      !addOn.classList.contains(".hidden")
+    ) {
+      isMonthly();
+    } else if (
+      addOn.classList.contains("yearly-cost") &&
+      !addOn.classList.contains("hidden")
+    ) {
+      isYearly();
+    }
+  });
 }
+
 // EVENT LISTENERS ON PAGE
 document.addEventListener("DOMContentLoaded", (e) => {
   showStep(currentStep);
@@ -249,6 +356,7 @@ next2.forEach((btn) => {
 });
 next3.forEach((btn) => {
   btn.addEventListener("click", () => {
+    validateStep3();
     nextStep();
   });
 });
@@ -264,14 +372,59 @@ backBtns.forEach((btn) => {
 toggleBtn.addEventListener("click", () => {
   const monthly = document.querySelectorAll(".monthly-sub");
   const yearly = document.querySelectorAll(".yearly-sub, .yearly");
+  const yearlyAddOn = document.querySelectorAll(".yearly-add");
+  const monthlyAddOn = document.querySelectorAll(".monthly-add");
+
+  function toggleSub() {
+    monthlyAddOn.forEach((on) => {
+      on.classList.toggle("hidden");
+    });
+    yearlyAddOn.forEach((on) => {
+      on.classList.toggle("hidden");
+    });
+  }
   monthly.forEach((month) => {
     month.classList.toggle("hidden");
+    toggleSub();
   });
   yearly.forEach((year) => {
     year.classList.toggle("hidden");
+    toggleSub();
+  });
+});
+function showError(plan) {
+  plan.style.border = "1px solid var(--primary-red-500)";
+}
+function removeError(plan) {
+  plan.style.border = "";
+}
+plans.forEach((plan) => {
+  plan.addEventListener("click", () => {
+    if (plan === arcade) {
+      arcade.classList.add("selected-plan");
+      advanced.classList.remove("selected-plan");
+      pro.classList.remove("selected-plan");
+      removeError(arcade);
+      removeError(advanced);
+      removeError(pro);
+    } else if (plan === advanced) {
+      advanced.classList.add("selected-plan");
+      arcade.classList.remove("selected-plan");
+      pro.classList.remove("selected-plan");
+      removeError(arcade);
+      removeError(advanced);
+      removeError(pro);
+    } else {
+      pro.classList.add("selected-plan");
+      arcade.classList.remove("selected-plan");
+      advanced.classList.remove("selected-plan");
+      removeError(arcade);
+      removeError(advanced);
+      removeError(pro);
+    }
   });
 });
 // STEP 3 EVENT LISTENERS
-checkbox.forEach((check) => {
-  check.addEventListener("click", () => {});
-});
+// checkbox.forEach((check) => {
+//   check.addEventListener("click", () => {});
+// });
